78392479ae6fd8d3bbbba2d663f2f1758e0ce998
QuangVinh0808 <qvinh023@gmail.com>
2026-03-03 22:24:34 +0700

    Configure cameraPi2
    Configure cameraPi2

--- main.py
+++ main.py
@@ -1,6 +1,17 @@
 from core.preprocess import ImagePreprocessor
 
 
+#Configure instance of PiCamera2
+"""
+cam = Picamera2()
+## Set the resolution of the camera preview
+cam.preview_configuration.main.size = (640, 360)
+cam.preview_configuration.main.format = "RGB888"
+cam.preview_configuration.controls.FrameRate=30
+cam.preview_configuration.align()
+cam.configure("preview")
+cam.start()
+"""
 
 if __name__ == "__main__":
     preprocessor = ImagePreprocessor(gamma=1.2, clip_limit=2.0, tile_grid_size=(8, 8))
